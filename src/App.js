868c74de62165391984cb14262e3c1802d3596b8
[your <jessica.menezs@gmail.com>
2022-02-06 18:18:06 -0300

    done home cusine

--- src/App.js
+++ src/App.js
@@ -1,17 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header';
 import Search from './components/Search';
+import { useState } from 'react';
+import axios from 'axios';
+import { useEffect } from 'react';
+import Home from './components/Home';
+
 
 function App() {
+  const [recipes, setRecipes] = useState([]);
+  const getRecipes = async () => { 
+    const {data} = await axios.get('https://www.themealdb.com/api/json/v1/1/list.php?a=list')
+    setRecipes(data);
+  }
+
+  useEffect(() => {
+    getRecipes();
+  }, []);
+  
+  console.log(recipes);
+
+  if (!recipes){
+    return ''}
+  
+  //let recipeTest = recipes.meals.map(recipe => console.log(recipe.strIngredient))
+
   return (
     <div className="App">
     <Header/>
-    <div>
-      teste
-    </div>
     <Search/>
+    <Home/>
+    
+  
     </div>
+      
   );
 }
 
---
+++ src/components/Home.js
@@ -0,0 +1,102 @@
+import { Link } from "react-router-dom"
+import './Home.css'
+const Home = () => {
+    return(
+        <div>
+        <div className="home-row">
+        <Link to='/spanish' className="home-cusine">
+            <div className="cusine">
+            <p>Spanish</p>
+                <img className="cusine-img" src="https://images.unsplash.com/photo-1571167366136-b57e07761625?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1335&q=80" alt="spanish"/>
+
+            </div>
+        </Link>
+        <Link to='/thai' className="home-cusine">
+            <div className="cusine">
+            <p>Thai</p>
+                <img className="cusine-img" src="https://images.unsplash.com/photo-1511910849309-0dffb8785146?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1287&q=80" alt="thai"/>
+
+            </div>
+        </Link>
+        <Link to='/moroccan' className="home-cusine">
+            <div className="cusine">
+            <p>Moroccan</p>
+                <img className="cusine-img" src="https://images.unsplash.com/photo-1511690656952-34342bb7c2f2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80" alt="moroccan"/>
+
+            </div>
+        </Link>
+        </div>
+        <div className="home-row">
+        <Link to='/american' className="home-cusine">
+            <div className="cusine">
+            <p>American</p>
+                <img className="cusine-img" src="https://cdn.pixabay.com/photo/2016/03/26/23/19/hamburger-1281855_960_720.jpg" alt="american"/>
+
+            </div>
+        </Link>
+        <Link to='/british' className="home-cusine">
+            <div className="cusine">
+            <p>British</p>
+                <img className="cusine-img" src="https://images.unsplash.com/photo-1635897411141-7bd2b9c6ab16?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80" alt="british"/>
+
+            </div>
+        </Link>
+        <Link to='/canadian' className="home-cusine">
+            <div className="cusine">
+            <p>Canadian</p>
+                <img className="cusine-img" src="https://cdn.pixabay.com/photo/2019/04/10/00/59/french-fries-4115953_960_720.jpg" alt="canadian"/>
+
+            </div>
+        </Link>
+        </div>
+        <div className="home-row">
+        <Link to='/chinese' className="home-cusine">
+            <div className="cusine">
+            <p>Chinese</p>
+                <img className="cusine-img" src="https://images.unsplash.com/photo-1569718212165-3a8278d5f624?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80" alt="chinese"/>
+
+            </div>
+        </Link>
+        <Link to='/french' className="home-cusine">
+            <div className="cusine">
+            <p>French</p>
+                <img className="cusine-img" src="https://images.unsplash.com/photo-1549903072-7e6e0bedb7fb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1287&q=80" alt="french"/>
+
+            </div>
+        </Link>
+        <Link to='/italian' className="home-cusine">
+            <div className="cusine">
+            <p>Italian</p>
+                <img className="cusine-img" src="https://images.unsplash.com/photo-1546549032-9571cd6b27df?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1287&q=80" alt="italian"/>
+
+            </div>
+        </Link>
+        </div>
+        <div className="home-row">
+        <Link to='/japonese' className="home-cusine">
+            <div className="cusine">
+            <p>Japonese</p>
+                <img className="cusine-img" src="https://images.unsplash.com/photo-1579584425555-c3ce17fd4351?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=627&q=80" alt="japonese"/>
+
+            </div>
+        </Link>
+        <Link to='/mexican' className="home-cusine">
+            <div className="cusine">
+            <p>Mexican</p>
+                <img className="cusine-img" src="https://cdn.pixabay.com/photo/2020/04/21/14/44/mexican-food-5073328_960_720.jpg" alt="mexican"/>
+
+            </div>
+        </Link>
+        <Link to='/portuguese' className="home-cusine">
+            <div className="cusine">
+            <p>Portuguese</p>
+                <img className="cusine-img" src="https://images.unsplash.com/photo-1485921325833-c519f76c4927?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1364&q=80" alt="portuguese"/>
+
+            </div>
+        </Link>
+        </div>
+        
+        </div>
+    )
+}
+export default Home;
