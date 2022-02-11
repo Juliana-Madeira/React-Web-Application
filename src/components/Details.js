e27a1c8c04cb4bee4a5e0c58012e2e771caf6980
Sergio <juliwood1980@gmail.com>
2022-02-11 17:27:03 -0300

    details styles css

--- src/components/Details.js
+++ src/components/Details.js
@@ -30,38 +30,44 @@ const Details = () => {
       <Header />
       <Search />
       <div className="details">
-      <div className="details-pic-name">
-       <p>{recipe.strMeal}</p> 
-        <img src={recipe.strMealThumb} alt={recipe.strMeal} />
-      </div>
-      <div className="details-ingridients-instructions">
-        <div className="details-ingridients">
-          <div className="ingredients">
-        <p>{recipe.strMeasure1} {recipe.strIngredient1}</p>
-        <p>{recipe.strMeasure2} {recipe.strIngredient2}</p>
-        <p>{recipe.strMeasure3} {recipe.strIngredient3}</p>
-        <p>{recipe.strMeasure4} {recipe.strIngredient4}</p>
-        <p>{recipe.strMeasure5} {recipe.strIngredient5}</p>
-        <p>{recipe.strMeasure6} {recipe.strIngredient6}</p>
-        <p>{recipe.strMeasure7} {recipe.strIngredient7}</p>
-        <p>{recipe.strMeasure8} {recipe.strIngredient8}</p>
-        <p>{recipe.strMeasure9} {recipe.strIngredient9}</p>
-        <p>{recipe.strMeasure10} {recipe.strIngredient10}</p>
-          </div>
-          <div className="ingredients">
-        <p>{recipe.strMeasure11} {recipe.strIngredient11}</p>
-        <p>{recipe.strMeasure12} {recipe.strIngredient12}</p>
-        <p>{recipe.strMeasure13} {recipe.strIngredient13}</p>
-        <p>{recipe.strMeasure14} {recipe.strIngredient14}</p>
-        <p>{recipe.strMeasure15} {recipe.strIngredient15}</p>
-        <p>{recipe.strMeasure16} {recipe.strIngredient16}</p>
-        <p>{recipe.strMeasure17} {recipe.strIngredient17}</p>
-        <p>{recipe.strMeasure18} {recipe.strIngredient18}</p>
-        <p>{recipe.strMeasure19} {recipe.strIngredient19}</p>
-        <p>{recipe.strMeasure20} {recipe.strIngredient20}</p>
-          </div>
+        <div className="details-pic-name">
+          <p>{recipe.strMeal}</p> 
+          <img src={recipe.strMealThumb} alt={recipe.strMeal} />
         </div>
-        <p>{recipe.strInstructions}</p>
+        <hr style={{width: "800px"}}/>
+        <div >
+          <p className="title-ingredients">Ingredients</p>
+          <div className="details-ingridients">
+            <div className="ingredients">
+              <p>{recipe.strMeasure1} {recipe.strIngredient1}</p>
+              <p>{recipe.strMeasure2} {recipe.strIngredient2}</p>
+              <p>{recipe.strMeasure3} {recipe.strIngredient3}</p>
+              <p>{recipe.strMeasure4} {recipe.strIngredient4}</p>
+              <p>{recipe.strMeasure5} {recipe.strIngredient5}</p>
+              <p>{recipe.strMeasure6} {recipe.strIngredient6}</p>
+              <p>{recipe.strMeasure7} {recipe.strIngredient7}</p>
+              <p>{recipe.strMeasure8} {recipe.strIngredient8}</p>
+              <p>{recipe.strMeasure9} {recipe.strIngredient9}</p>
+              <p>{recipe.strMeasure10} {recipe.strIngredient10}</p>
+            </div>
+            <div className="ingredients">    
+              <p>{recipe.strMeasure11} {recipe.strIngredient11}</p>
+              <p>{recipe.strMeasure12} {recipe.strIngredient12}</p>
+              <p>{recipe.strMeasure13} {recipe.strIngredient13}</p>
+              <p>{recipe.strMeasure14} {recipe.strIngredient14}</p>
+              <p>{recipe.strMeasure15} {recipe.strIngredient15}</p>
+              <p>{recipe.strMeasure16} {recipe.strIngredient16}</p>
+              <p>{recipe.strMeasure17} {recipe.strIngredient17}</p>
+              <p>{recipe.strMeasure18} {recipe.strIngredient18}</p>
+              <p>{recipe.strMeasure19} {recipe.strIngredient19}</p>
+              <p>{recipe.strMeasure20} {recipe.strIngredient20}</p>
+            </div>
+          </div>
+        <hr style={{width: "800px"}}/>
+        <p className="title-ingredients">Instructions</p>
+        <div className="details-ingridients-instructions">
+          <p>{recipe.strInstructions}</p>
+        </div>
       </div>
       </div>
       <Footer/> 
