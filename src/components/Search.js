1e0dbc60e2760c69bfaf46168030973a0db46f06
JessicaNonato <jessica.menezs@gmail.com>
2022-02-12 13:51:15 -0300

    search

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,35 +1,27 @@
-import React, { useState, useEffect } from "react";
-import { Link, useParams } from "react-router-dom";
-import './Search.css';
-import axios from 'axios';
+import {useState} from "react";
+import { Link } from "react-router-dom";
+import "./Search.css";
 
 const Search = () => {
+  const [value, setValue] = useState("");
 
-    const strMealParams = useParams();
-  
-
-    const [meals, setMeals] = useState([]);
-
-    const getMeal = async () => {
-      const { data } = await axios.get(
-
-        `https://www.themealdb.com/api/json/v1/1/search.php?s=${strMealParams.strMeal}`
-      );
-      
-      setMeals(data.meals);
-    };
-  
-  
-    useEffect(() => {
-      getMeal();
-    }, []);
-  
-
-    return(
-        <div className="search">
-        <input type='text' placeholder='Search by Name of Food' className="input"/>
-        <Link to= {`/search`}><button type="button" className="button">Search</button></Link>
-        </div>
-    )
-}
+  return (
+    <form className="search">
+      <input
+        type="text"
+        placeholder="Search by Name of Food"
+        value={value}
+        onChange={(event) => setValue(event.target.value)}
+        className="input"
+      />
+      <Link to={`/search/${value}`}>
+        <button 
+        type="button" 
+        className="button">
+          Search
+        </button>
+      </Link>
+    </form>
+  );
+};
 export default Search;
