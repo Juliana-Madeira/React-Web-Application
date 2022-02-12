e5fc1a4b9de82fc14b261e702f283a1981f24b7e
JessicaNonato <jessica.menezs@gmail.com>
2022-02-12 16:07:39 -0300

    header

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,9 +8,33 @@ import './Header.css'
 const Header = () =>{
 return(
     <div className="header">
+        <div className="category-row">
+    <Link className='category' to='/categories/Vegetarian'>
+    <h4>Vegetarian</h4>
+    </Link>
+    <Link className='category' to='/categories/Vegan'>
+    <h4>Vegan</h4>
+    </Link>
+    <Link className='category' to='/categories/Dessert'>
+    <h4>Dessert</h4>
+    </Link>
+    </div>
     <Link className='logo' to='/'>
     <img src={Spork} alt="the-spork-logo"/>
     </Link>
+    <div className="category-row">
+    <Link className='category' to='/categories/about'>
+    <h4>About</h4>
+    </Link>
+    <Link className='category' to='/categories/ingridients'>
+    <h4>Ingridients</h4>
+    </Link>
+    <Link className='category' to='/categories/Favorits'>
+    <h4>Favorits</h4>
+    </Link>
+    </div>
+    
+   
            
     </div>
 )
