cda392407ca0953ae5e3391662d329ff949078aa
JessicaNonato <jessica.menezs@gmail.com>
2022-02-19 09:55:18 -0300

    about

--- src/components/About.js
+++ src/components/About.js
@@ -1,18 +1,21 @@
 import React from 'react';
+import Header from './Header';
+import Footer from './Footer';
+
 
 const About = () => {
     return (
         <div>
             <Header/>
-            <Search/>
-            {/*<span>This is the Conclusion Project of the Second Module of Bootcamp, 
+            <span>This is the Conclusion Project of the Second Module of Bootcamp, 
             made at Ironhack in São Paulo/Brazil.</span>
             <p><span>We use Javascript, React JS, CSS3 and HTML5, in all the work.</span></p>
             <p><span>With requests made to the API “The Meal DB”, we were able to use recipes, 
-            images and many other data.<span></p>
-            <p><span>It was a great learning experience, as well as great fun with the recipes we got to know.<span></p>
-            <p><span>Project developed by Juliana Madeira and Jessica Nonato.<span></p>*/}
+            images and many other data.</span></p>
+            <p><span>It was a great learning experience, as well as great fun with the recipes we got to know.</span></p>
+            <p><span>Project developed by Juliana Madeira and Jessica Nonato.</span></p>
             <Footer/>
         </div>
     )
 }
+export default About;
