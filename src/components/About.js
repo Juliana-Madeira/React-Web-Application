2bd2f8da2d9a3aded031332b2ce342a116fcaac4
Sergio <juliwood1980@gmail.com>
2022-02-19 13:56:18 -0300

    about css

--- src/components/About.js
+++ src/components/About.js
@@ -18,8 +18,8 @@ const About = () => {
             <p><span>It was a great learning experience, as well as great fun with the recipes we got to know.</span></p>
             <p><span>Project developed by <a href='https://www.linkedin.com/in/juliana-g-madeira/'className='creators-link'>Juliana Madeira</a> and <a  href='https://www.linkedin.com/in/jessica-nonato/' className='creators-link'>Jessica Nonato</a>.</span></p>
             <p><span>See creators GitHub:</span></p>
-            <p><span><GoMarkGithub/> <a href='https://github.com/Juliana-Madeira'>Juliana Madeira</a></span></p>
-            <p><span><GoMarkGithub/> <a href='https://github.com/JessicaNonato'>Jessica Nonato</a></span></p>
+            <p><span><GoMarkGithub/> <a href='https://github.com/Juliana-Madeira' className='creators-link'>Juliana Madeira</a></span></p>
+            <p><span><GoMarkGithub/> <a href='https://github.com/JessicaNonato' className='creators-link'>Jessica Nonato</a></span></p>
             </div>
             <Footer/>
         </div>
